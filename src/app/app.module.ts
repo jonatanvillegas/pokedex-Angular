a6e69cc84b14ce8077b01da7f8f2c35bc9cdac40
jonatan <villegasjoncena7@gmail.com>
2024-02-05 21:39:50 -0600

    bases del pokedex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FotoPokemonComponent } from './components/foto-pokemon/foto-pokemon.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FotoPokemonComponent,
+    TarjetaComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
